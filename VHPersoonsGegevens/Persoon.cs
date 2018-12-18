2a6adc72ff9a087c1b57c63bbde4bfe8d309f50d
Arthur Schut <aschutgh@gmail.com>
2018-12-18 11:07:23 +0100

    Completed GUI and added Persoon class

---
+++ VHPersoonsGegevens/Persoon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VHPersoonsGegevens
+{
+    class Persoon
+    {
+        // Properties
+        // FIXME: Modify this stuff and use English
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public GeslachtEnum Geslacht { get; set; }
+        public DateTime GeboorteDatum { get; set; }
+        public string Land { get; set; }
+    }
+
+
+    enum GeslachtEnum
+    {
+        Man, Vrouw, Onbekend
+    }
+}
